a3cb553fde37d01c0491c06b8f506b762f29872f
Radoslaw-Wolnik <radoslaw.m.wolnik@gmail.com>
2024-04-29 20:01:04 +0200

    added reprime to try to reprime M but is not working for test data

--- 06_Chinskie_tw_reszty_Euler/main.py
+++ 06_Chinskie_tw_reszty_Euler/main.py
@@ -9,8 +9,11 @@ def gcd(a, b):
         a, b = b, a % b
     return a
 
+
 def coprime(modulos):
     """Check if the given modulos are pairwise coprime."""
+    if modulos is None:
+        return False
     n = len(modulos)
     for i in range(n):
         for j in range(i + 1, n):
@@ -18,15 +21,16 @@ def coprime(modulos):
                 return False
     return True
 
+
 def chinese_remainder_theorem(remainders, modulos):
     """
     Chinese Remainder Theorem implementation to find the value of 'a' that satisfies the system of equations.
     """
     assert len(remainders) == len(modulos), "The number of remainders must be equal to the number of modulos."
-    
+
     if not coprime(modulos):
         return None
-    
+
     M = 1
     for modulo in modulos:
         M *= modulo
@@ -38,6 +42,34 @@ def chinese_remainder_theorem(remainders, modulos):
         result += remainder * Mi * Mi_inv
 
     return result % M
+
+
+def gcd_list(numbers):
+    if not numbers:
+        return None  # If the list is empty, there's no common divisor
+    result = numbers[0]
+    for num in numbers[1:]:
+        result = gcd(result, num)
+    return result
+
+
+def reprime(A, M):
+    mod = gcd_list(M)
+    if mod == 1:
+        return None, A
+    same = [a % mod for a in A]
+    is_the_same = same[0]  # some number that we will be comparing to
+    for i in range(len(same)):
+        if same[i] != same[0]:
+            is_the_same = -1  # False
+            break
+    if is_the_same < 0:
+        return None, A
+    # zeby zrobic w petli to na pierwsze miejsce bierzemy ostatni element, potem (pierwszy element / gcd(list)) i potem reszte listy
+    if len(M) > 2:
+        return [M[-1]] + [M[0] / mod] + M[1:-1], [A[-1], A[0]] + A[1:-1]
+    else:
+        return [M[-1]] + [M[0] / mod], [A[-1], A[0]]
 
 
 if __name__ == '__main__':
@@ -52,9 +84,18 @@ if __name__ == '__main__':
     # M = [int(num) for num in M]
     A = [2, 3, 5]
     M = [3, 5, 7]
-    A, M = (A, M)
-    result = chinese_remainder_theorem(A, M)
-    if result == None:
+    result = None
+    if coprime(M) is False:
+        M, A = reprime(A, M)
+        while not coprime(M):
+            if M is None:
+                break
+            M, A = reprime(A, M)
+        if M is not None:
+            result = chinese_remainder_theorem(A, M)
+    else:
+        result = chinese_remainder_theorem(A, M)
+    if result is None:
         print("Brak rozwiązania!!!")
     else:
         # print("Value of 'a' satisfying the system of equations:", result)
